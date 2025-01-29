c8083121127e4fc6addacba5ad920b3038155ed7
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-29 20:15:23 -0300

    Agregar vista para nuevo producto y enlace de navegación en ProductosView

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -1,12 +1,11 @@
 <script setup>
 import NavlinkComp from '@/components/navigation/NavlinkComp.vue';
-
 </script>
 
 <template>
   <div>
-    <NavlinkComp url-name="new-product">Volver</NavlinkComp>
-    <h1 class="text-4xl font-black mt-4">Productos</h1>
+    <NavlinkComp url-name="products">Volver</NavlinkComp>
+    <h1 class="text-4xl font-black mt-4">Nuevo producto</h1>
   </div>
 </template>
 
