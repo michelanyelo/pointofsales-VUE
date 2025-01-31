1e68eaedfd4362437cf3ab6d8149810eca49cc9b
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-30 21:42:31 -0300

    Agregar formulario para nuevo producto en la vista de administración con validaciones

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -5,11 +5,31 @@ import NavlinkComp from '@/components/navigation/NavlinkComp.vue';
 <template>
   <div>
     <NavlinkComp url-name="products">Volver</NavlinkComp>
-    <h1 class="text-4xl font-black mt-4">Nuevo producto</h1>
+    <h1 class="text-4xl font-black my-10">Nuevo producto</h1>
 
-    <FormKit type="form">
-      <FormKit type="text" />
-    </FormKit>
+    <div class="flex justify-center bg-white shadow">
+      <div class="mt-10 p-10 w-full 2xl:w-2/4">
+        <FormKit type="form" submit-label="Agregar">
+          <!-- nombre del producto -->
+          <FormKit type="text" label="Nombre" name="nombre" placeholder="Nombre del prpducto" validation="required"
+            :validation-messages="{ required: 'El campo nombre es obligatorio' }" />
+          <!-- imagen del producto -->
+          <FormKit type="file" label="Imagen" name="image" validation="required"
+            :validation-messages="{ required: 'El campo imagen es obligatorio' }" accept=".jpg, .png, .jpeg"
+            multiple="true" />
+          <!-- categoria del producto -->
+          <FormKit type="select" label="Categoría" name="category" validation="required"
+            :validation-messages="{ required: 'El campo categoría es obligatorio' }" :options="[1, 2, 3]" />
+          <!-- precio del producto -->
+          <FormKit type="number" label="Precio" name="price" placeholder="Precio del prpducto" validation="required"
+            :validation-messages="{ required: 'El campo precio es obligatorio' }" min="1" />
+          <!-- stock del producto -->
+          <FormKit type="number" label="Disponibles" name="stock" placeholder="Cantidad disponible"
+            validation="required" :validation-messages="{ required: 'El campo disponible es obligatorio' }" min="1" />
+        </FormKit>
+      </div>
+    </div>
+
   </div>
 </template>
 
