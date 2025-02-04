e45928f1cc096dc4157f9b0bd2b2b1a592665033
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-02-04 18:33:10 -0300

    Agregar configuración de FormKit y dependencia de TailwindCSS para formularios

--- src/main.js
+++ src/main.js
@@ -2,15 +2,21 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { plugin, defaultConfig } from '@formkit/vue'
-// import defaultConfig from '../formkit.config'
-
+import { rootClasses } from '../formkit.theme.mjs'
 import App from './App.vue'
 import router from './router'
+
+// Configuración de FormKit
+const config = defaultConfig({
+  config: {
+    rootClasses: rootClasses,
+  },
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(plugin, defaultConfig)
+app.use(plugin, config)
 app.use(router)
 
 app.mount('#app')
