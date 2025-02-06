10a24b853d9a081bb9022d806067ed55adfc5c51
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-02-06 16:18:56 -0300

    Agregar vista Seeder para cargar productos en la base de datos y actualizar rutas en el enrutador

---
+++ src/data/products.js
@@ -0,0 +1,242 @@
+export const products = [
+  {
+    "name": "Sudadera Deportiva Gris",
+    "price": 49,
+    "stock": 5,
+    "category": 1
+  },
+  {
+    "name": "Sudadera Casual Naranja para Hombre",
+    "price": 29,
+    "stock": 3,
+    "category": 1
+  },
+  {
+    "name": "Sudadera Básica Azul",
+    "price": 49,
+    "stock": 8,
+    "category": 1
+  },
+  {
+    "name": "Sudadera Blanca Clásica",
+    "price": 29,
+    "stock": 10,
+    "category": 1
+  },
+  {
+    "name": "Sudadera Bicolor Negra y Naranja",
+    "price": 49,
+    "stock": 10,
+    "category": 1
+  },
+  {
+    "name": "Sudadera Negra con Detalles Rojos",
+    "price": 39,
+    "stock": 10,
+    "category": 1
+  },
+  {
+    "name": "Sudadera Roja Premium",
+    "price": 49,
+    "stock": 10,
+    "category": 1
+  },
+  {
+    "name": "Sudadera Blanco con Azul",
+    "price": 49,
+    "stock": 10,
+    "category": 1
+  },
+  {
+    "name": "Sudadera Amarilla para Mujer",
+    "price": 39,
+    "stock": 10,
+    "category": 1
+  },
+  {
+    "name": "Zapatillas Urbanas para Mujer",
+    "price": 59,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapato Casual de Piel",
+    "price": 49,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Rosadas para Mujer",
+    "price": 49,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Running Grises",
+    "price": 79,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Deportivas Rojo y Negro",
+    "price": 69,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas de Deporte para Mujer",
+    "price": 69,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Running Azul Hombre",
+    "price": 69,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Urbanas Azul",
+    "price": 69,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Casual Hombre",
+    "price": 59,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Grises Casual",
+    "price": 49,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Botines Claros Urbanos",
+    "price": 59,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Botines Negros de Cuero",
+    "price": 59,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Botines Negro con Azul",
+    "price": 59,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Negras para Hombre",
+    "price": 49,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Gris y Negro Casual",
+    "price": 49,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Negras para Hombre",
+    "price": 59,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Running Gris para Hombre",
+    "price": 69,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatos Grises para Hombre",
+    "price": 69,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Deportivas Gris con Rojo",
+    "price": 49,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Negras para Mujer",
+    "price": 59,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Azul Casual",
+    "price": 69,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Running Gris Deportivas",
+    "price": 69,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Verde Casual",
+    "price": 59,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Zapatillas Rojo Casual",
+    "price": 59,
+    "stock": 10,
+    "category": 2
+  },
+  {
+    "name": "Lentes de Sol Polarizados - Gris",
+    "price": 39,
+    "stock": 10,
+    "category": 3
+  },
+  {
+    "name": "Lentes de Sol Clásicos - Negro",
+    "price": 39,
+    "stock": 10,
+    "category": 3
+  },
+  {
+    "name": "Gafas de Sol Oscuras",
+    "price": 29,
+    "stock": 10,
+    "category": 3
+  },
+  {
+    "name": "Gafas de Sol con Protección UV",
+    "price": 49,
+    "stock": 10,
+    "category": 3
+  },
+  {
+    "name": "Lentes de Sol con Acabado Azul",
+    "price": 39,
+    "stock": 10,
+    "category": 3
+  },
+  {
+    "name": "Gafas Modernas de Sol - Negro",
+    "price": 39,
+    "stock": 10,
+    "category": 3
+  },
+  {
+    "name": "Lentes de Sol Estilo Clásico",
+    "price": 39,
+    "stock": 10,
+    "category": 3
+  }
+];
