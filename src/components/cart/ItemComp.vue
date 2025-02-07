0725198b3a83e015078afba98b4c41e0419e527a
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-02-06 21:26:14 -0300

    Agregar selector de cantidad en ItemComp para permitir la selección de la cantidad de productos en el carrito.

--- src/components/cart/ItemComp.vue
+++ src/components/cart/ItemComp.vue
@@ -13,6 +13,10 @@ defineProps({
     <div class="flex-auto space-y-2">
       <h3 class="text-gray-900">{{ item.name }}</h3>
       <p>{{ formatCurrency(item.price) }}</p>
+
+      <select name="amount" class="w-48 text-center p-1 rounded-lg bg-white">
+        <option v-for="i in 5" :value="i">{{ i }}</option>
+      </select>
     </div>
   </li>
 </template>
