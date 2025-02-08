b260043af01a7d612967870f812684e4f036769a
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-02-08 12:29:05 -0300

    Actualizar componente ItemComp para vincular la cantidad seleccionada al estado del carrito y mejorar el cálculo del subtotal en el store.

--- src/components/cart/ItemComp.vue
+++ src/components/cart/ItemComp.vue
@@ -1,11 +1,8 @@
 <script setup>
 import { useCartStore } from '@/stores/cart';
 import { formatCurrency } from '@/helpers/currency';
-import { ref } from 'vue';
 
 const cartStore = useCartStore();
-
-const selectedQuantity = ref(1);
 
 defineProps({
   item: Object
@@ -20,9 +17,9 @@ defineProps({
       <h3 class="text-gray-900">{{ item.name }}</h3>
       <p>{{ formatCurrency(item.price) }}</p>
 
-      <select name="amount" class="w-48 text-center p-1 rounded-lg bg-white" v-model="selectedQuantity"
-        @change="cartStore.updateQuantity(item.id, selectedQuantity)">
-        <option v-for="i in cartStore.checkProductStock(item)" :value="i">{{ i }}</option>
+      <select name="amount" class="w-48 text-center p-1 rounded-lg bg-white"
+        @change="cartStore.updateQuantity(item.id, +$event.target.value)" :value="item.quantity">
+        <option v-for="i in cartStore.checkProductStock(item)" :key="i" :value="i">{{ i }}</option>
       </select>
     </div>
   </li>
--- src/stores/cart.js
+++ src/stores/cart.js
@@ -9,20 +9,28 @@ export const useCartStore = defineStore('cart', () => {
 
   const max_products = 5
 
+  watchEffect(() => {
+    subtotal.value = items.value.reduce((total, item) => total + (item.quantity * item.price), 0)
+    totalQuantity.value = items.value.reduce((total, item) => total + item.quantity, 0)
+  });
+
   function addItem(item) {
-    items.value.push({ ...item, quantity: 1, id: item.id });
+    const index = isItemInCart(item.id)
+
+    if (index >= 0) {
+      items.value[index].quantity++;
+    } else {
+      items.value.push({ ...item, quantity: 1, id: item.id });
+    }
   }
 
   function updateQuantity(id, quantity) {
     items.value = items.value.map(item => item.id === id ? { ...item, quantity } : item);
   }
 
-  watchEffect(() => {
-    subtotal.value = items.value.reduce((total, item) => total + (item.quantity * item.price), 0)
-    totalQuantity.value = items.value.reduce((total, item) => total + item.quantity, 0)
-  });
-
   const isEmpty = computed(() => items.value.length === 0);
+
+  const isItemInCart = id => items.value.findIndex(item => item.id === id);
 
   const checkProductStock = computed(() => {
     return (product) => product.stock < max_products ? product.stock : max_products;
